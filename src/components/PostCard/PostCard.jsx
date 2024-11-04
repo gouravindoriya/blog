0528538e64ba4cda35de10473e95307254c16fb2
gouravindoriya <gouravindoriya00@gmail.com>
2024-11-04 15:39:43 +0530

    introduces favicon and title of the page

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -7,10 +7,10 @@ const PostCard = ({$id,title,featureimage}) => {
   return (
     <Link to={`/post/${$id}`}>
      <div className='w-full p-4 rounded-md'>
-       <div>
+       
         
-        <img className='w-full rounded-md' src={service.previewfile(featureimage)} alt={title} />
-       </div>
+        <img className='min-w-56  rounded-md' src={service.previewfile(featureimage)} alt={title} />
+       
        <h2>{title}</h2>
 
      </div>
--- src/components/pages/Homecomponent.jsx
+++ src/components/pages/Homecomponent.jsx
@@ -31,7 +31,7 @@ function Homecomponent() {
     return (
         <div className='w-full py-8'>
             <Container>
-                <div className='flex flex-wrap'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4'>
                     {posts.map((post) => (
                         <div key={post.$id} className='p-2 w-1/4'>
                             <PostCard {...post} />
